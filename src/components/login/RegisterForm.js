87f1258ec7c55ccc26c5f182b2400610fcb49d3d
DevThomas <fremen1990@gmail.com>
2022-07-04 20:15:50 +0200

    Dates and gender options and functionality

--- src/components/login/RegisterForm.js
+++ src/components/login/RegisterForm.js
@@ -2,19 +2,40 @@ import { Form, Formik } from 'formik'
 import RegisterInput from '../inputs/registerInput'
 import { useState } from 'react'
 
-const userInfos = {
-    first_name: '',
-    last_name: '',
-    email: '',
-    password: '',
-    bYear: '',
-    bMonth: '',
-    bDay: '',
-    gender: '',
-}
-
 function RegisterForm(props) {
+    const userInfos = {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        bYear: new Date().getFullYear(),
+        bMonth: new Date().getMonth() + 1,
+        bDay: new Date().getDate(),
+        gender: '',
+    }
+
     const [user, setUser] = useState(userInfos)
+    const {
+        first_name,
+        last_name,
+        email,
+        password,
+        bYear,
+        bMonth,
+        bDay,
+        gender,
+    } = user
+
+    // Generating new Arreas with year month and day
+    const yearTemp = new Date().getFullYear()
+    const years = Array.from(new Array(108), (val, index) => yearTemp - index)
+    const months = Array.from(new Array(12), (val, index) => 1 + index)
+    const getDays = () => {
+        return new Date(bYear, bMonth, 0).getDate()
+    }
+    const days = Array.from(new Array(getDays()), (val, index) => 1 + index)
+
+    console.log(user)
 
     const handleRegisterChange = (e) => {
         const { name, value } = e.target
@@ -67,14 +88,38 @@ function RegisterForm(props) {
                                         <i className="info_icon"></i>
                                     </div>
                                     <div className="reg_grid">
-                                        <select name="bDay">
-                                            <option>12</option>
+                                        <select
+                                            value={bDay}
+                                            name="bDay"
+                                            onChange={handleRegisterChange}
+                                        >
+                                            {days.map((day, i) => (
+                                                <option value={day} key={i}>
+                                                    {day}
+                                                </option>
+                                            ))}
                                         </select>
-                                        <select name="bMonth">
-                                            <option>12</option>
+                                        <select
+                                            name="bMonth"
+                                            value={bMonth}
+                                            onChange={handleRegisterChange}
+                                        >
+                                            {months.map((month, i) => (
+                                                <option value={month} key={i}>
+                                                    {month}
+                                                </option>
+                                            ))}{' '}
                                         </select>
-                                        <select name="bYear">
-                                            <option>12</option>
+                                        <select
+                                            name="bYear"
+                                            value={bYear}
+                                            onChange={handleRegisterChange}
+                                        >
+                                            {years.map((year, i) => (
+                                                <option value={year} key={i}>
+                                                    {year}
+                                                </option>
+                                            ))}{' '}
                                         </select>
                                     </div>
                                 </div>
