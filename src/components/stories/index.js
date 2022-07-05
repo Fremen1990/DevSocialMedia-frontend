df28d88810867c9fb38593a9446832edf082187b
DevThomas <fremen1990@gmail.com>
2022-07-06 00:18:12 +0200

    Stories component completed.

---
+++ src/components/stories/index.js
@@ -0,0 +1,27 @@
+import './style.css'
+import Story from './Story'
+import { ArrowRight, Plus } from '../../svg'
+import { stories } from '../../data/home'
+export default function Stories() {
+    return (
+        <div className="stories">
+            <div className="create_story_card">
+                <img
+                    src="/images/default_pic.png"
+                    alt="Story image"
+                    className="create_story_img"
+                />
+                <div className="plus_story">
+                    <Plus color="#fff" />
+                </div>
+                <div className="story_create_text">Create Story</div>
+            </div>
+            {stories.map((story, i) => (
+                <Story story={story} key={i} />
+            ))}
+            <div className="white_circle">
+                <ArrowRight color="#65676b" />
+            </div>
+        </div>
+    )
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,19 @@
+import './style.css'
 import Header from '../../components/header'
 import LeftHome from '../../components/home/left'
 import { useSelector } from 'react-redux'
 import RightHome from '../../components/home/right'
+import Stories from '../../components/stories'
 
 function Home() {
     const { user } = useSelector((user) => ({ ...user }))
     return (
-        <div>
+        <div className="home">
             <Header />
             <LeftHome user={user} />
+            <div className="home_middle">
+                <Stories />
+            </div>
             <RightHome user={user} />
         </div>
     )
