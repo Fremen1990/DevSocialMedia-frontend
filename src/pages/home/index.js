738861f743e293feab70e07b412fe76b44229761
DevThomas <fremen1990@gmail.com>
2022-07-05 23:45:15 +0200

    rightHome completed

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import Header from '../../components/header'
 import LeftHome from '../../components/home/left'
 import { useSelector } from 'react-redux'
+import RightHome from '../../components/home/right'
 
 function Home() {
     const { user } = useSelector((user) => ({ ...user }))
@@ -8,6 +9,7 @@ function Home() {
         <div>
             <Header />
             <LeftHome user={user} />
+            <RightHome user={user} />
         </div>
     )
 }
