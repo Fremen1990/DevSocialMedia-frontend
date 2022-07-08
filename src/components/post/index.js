48158426e3067a839fb20cf3cfac5aeeef76410f
DevThomas <fremen1990@gmail.com>
2022-07-08 03:43:59 +0200

    Post component completed with user data.
    Signed-off-by: DevThomas <fremen1990@gmail.com>

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,5 +1,48 @@
 import './style.css'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
+import { Dots, Public } from '../../svg'
 
 export default function Post({ post }) {
-    return <div>{post.text}</div>
+    return (
+        <div className="post">
+            <div className="post_header">
+                <Link
+                    to={`/profile/${post.user.username}`}
+                    className="post_header_left"
+                >
+                    <img src={post.user.picture} alt="user picture" />
+                    <div className="header_col">
+                        <div className="post_profile_name">
+                            {post.user.first_name} {post.user.last_name}
+                            <div className="updated_p">
+                                {post.type === 'profilePicture' &&
+                                    `updated ${
+                                        post.user.gender === 'male'
+                                            ? 'his'
+                                            : 'her'
+                                    } profile picture`}
+
+                                {post.type === 'cover' &&
+                                    `updated ${
+                                        post.user.gender === 'male'
+                                            ? 'his'
+                                            : 'her'
+                                    } cover picture`}
+                            </div>
+                        </div>
+                        <div className="post_profile_privacy_date">
+                            <Moment fromNow interval={30}>
+                                {post.createdAt}
+                            </Moment>
+                            <Public color="#828387" />
+                        </div>
+                    </div>
+                </Link>
+                <div className="post_header_right hover1">
+                    <Dots color="#828387" />
+                </div>
+            </div>
+        </div>
+    )
 }
