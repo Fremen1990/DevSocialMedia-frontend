198719a9157d4e3940a744ca3952baa9ad927daa
DevThomas <fremen1990@gmail.com>
2022-07-11 20:08:27 +0200

    Reacts functionality completed from backend and frontend as well.
    Signed-off-by: DevThomas <fremen1990@gmail.com>

--- src/components/post/ReactsPopup.js
+++ src/components/post/ReactsPopup.js
@@ -26,7 +26,7 @@ const reactsArray = [
     },
 ]
 
-export default function ReactsPopup({ visible, setVisible }) {
+export default function ReactsPopup({ visible, setVisible, reactHandler }) {
     return (
         <>
             {visible && (
@@ -44,7 +44,11 @@ export default function ReactsPopup({ visible, setVisible }) {
                     }}
                 >
                     {reactsArray.map((react, i) => (
-                        <div className="react" key={i}>
+                        <div
+                            className="react"
+                            key={i}
+                            onClick={() => reactHandler(react.name)}
+                        >
                             <img src={react.image} alt="react icon" />
                         </div>
                     ))}
